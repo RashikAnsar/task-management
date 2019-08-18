95dd290eea8cb97ebbfec2176b4c08f90465f3c4
RashikAnsar <sk.ansar46@gmail.com>
2019-08-18 10:45:54 +0530

    Create task functionality

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,5 +1,7 @@
+import * as uuid from 'uuid/v1';
+
 import { Injectable } from '@nestjs/common';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
 
 @Injectable()
 export class TasksService {
@@ -7,5 +9,17 @@ export class TasksService {
 
     getAllTasks(): Task[] {
         return this.tasks;
+    }
+
+    createTask(title: string, description: string) {
+        const task: Task = {
+            id: uuid(),
+            title,
+            description,
+            status: TaskStatus.OPEN
+        };
+
+        this.tasks.push(task);
+        return task;
     }
 }
