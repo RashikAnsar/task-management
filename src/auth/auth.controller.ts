5bbc181206ed817d66462bc83be6ff4cc4e700cc
RashikAnsar <sk.ansar46@gmail.com>
2019-08-23 11:47:00 +0530

    username password validation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { AuthService } from './auth.service';
 
@@ -7,7 +7,7 @@ export class AuthController {
     constructor(private authService: AuthService) { }
 
     @Post('/signup')
-    signUp(@Body() authCredentialDto: AuthCredentialsDto): Promise<void> {
+    signUp(@Body(ValidationPipe) authCredentialDto: AuthCredentialsDto): Promise<void> {
         return this.authService.signUp(authCredentialDto);
     }
 }
