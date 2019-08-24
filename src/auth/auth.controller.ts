bfb7f758b82b0f31fbcd765d38dd501fe69e357b
RashikAnsar <sk.ansar46@gmail.com>
2019-08-24 18:50:58 +0530

    sign the token upon authentication

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -12,7 +12,7 @@ export class AuthController {
     }
 
     @Post('/signin')
-    signIn(@Body() authCredentialsDto: AuthCredentialsDto) {
+    signIn(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto): Promise<{accessToken: string}> {
         return this.authService.singIn(authCredentialsDto);
     }
 }
