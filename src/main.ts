2bf472276f1e6687e8b8ad8fc393f47a942c1051
RashikAnsar <sk.ansar46@gmail.com>
2019-08-25 17:33:55 +0530

    NODE_ENV variables

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,13 @@ async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
 
+  if (process.env.NODE_ENV == 'development') {
+    app.enableCors();
+  } else {
+    app.enableCors({ origin: serverConfig.origin })
+    logger.log(`Accepting requests from origin "${serverConfig.origin}"`);
+  }
+
   const PORT = process.env.PORT || serverConfig.port;
   await app.listen(PORT);
 
